1b59fee18ab694f4522751c80ce669bfc76cfc59
Omar Munchaev <mr.munchaev@mail.ru>
2020-05-12 02:36:16 +0300

    sddssdsdds

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'y!91-bo#2ud!jzz#^x-s-y5j$yd5+n
 #DEBUG = True
 
 #DEBUG = bool( os.environ.get('DJANGO_DEBUG', False) )
-Debu = True
+DEBUG = True
 ALLOWED_HOSTS = [ 'rocky-badlands-33936.herokuapp.com', '192.168.1.3']
 
 
